35018ec6bbc7797416e89df332d996d449622092
cherrie235 <mathias.cherrier@gmail.com>
2025-03-05 10:01:31 +0100

    td3 vue

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,32 @@
 <script setup>
-import { ref , computed } from "vue";
+import { ref, computed } from "vue";
+import PostCard from "@/components/PostCard.vue";
 
 const text = ref("booh");
-const trimmedText = computed(()=>text.value.trim());
+const trimmedText = computed(() => text.value.trim());
 const posts = ref([]);
-const sortedPost = computed(()=> posts.value.sort((a,b)=>a.createdAt < b.createdAt));
+const sortedPost = computed(() => [...posts.value].sort((a, b) => b.createdAt - a.createdAt));
 function addPost() {
   const newPost = {
     id: Math.random().toString(36).substring(2),
     content: trimmedText.value,
     createdAt: new Date(),
-    author : {
-      username: "pyke",
-      avatarUrl: "https://statics.koreanbuilds.net/tile_200x200/Pyke.webp"
-    }
-    
-
-  }
+    author: {
+      username: "poubelles",
+      avatarUrl: "https://media1.tenor.com/m/2GwvVXLsJ4QAAAAd/trash-cans-dancing-trash-cans.gif",
+      //avatarUrl: "https://statics.koreanbuilds.net/tile_200x200/Pyke.webp"
+    },
+    liked: false
+  };
   posts.value.push(newPost);
   text.value = "";
+}
+function deletePost(id) {
+  posts.value = posts.value.filter((post) => post.id !== id);
+}
+function like(id){
+  posts.value.find((post)=> post.id == id).liked ^= true;
+  
 }
 </script>
 
@@ -31,74 +39,13 @@ function addPost() {
       </form>
 
       <h2 v-if="!posts.length">Aucun post pour le moment.</h2>
-
-      <article v-for="(post, index) in sortedPost" :key="index" class="card">
-        <header>
-          <img :src="post.author.avatarUrl" alt="avatar" width="36" height="36">
-          <a>{{ post.author.username }}</a>
-        </header>
-        <p>{{ post.content }}</p>
-      </article>
+      <PostCard
+        v-for="(post, index) in sortedPost"
+        :key="index"
+        :post="post"
+        @delete="deletePost"
+        @like="like"
+      />
     </div>
   </main>
 </template>
-
-<style scoped>
-.container {
-  height: 100vh;
-  margin: 0 auto;
-  max-width: 640px;
-}
-.card { 
-  background-color: var(--color-bg-secondary);
-  border-radius: 10px;
-  border: 1px solid var(--color-border);
-  margin-bottom: 1rem;
-}
-
-form {
-  display: flex;
-  flex-direction: column;
-  margin-top: 1rem;
-  padding: 1rem 1.5rem;
-  width: 100%;
-}
-textarea {
-  background: none;
-  border: none;
-  color: var(--color-text-primary);
-  flex: 1;
-  margin-bottom: 1rem;
-  outline: none;
-  padding: 0.5rem 0;
-  resize: none;
-  field-sizing: content;
-}
-button {
-  align-self: flex-end;
-  background: none;
-  border-radius: 10px;
-  border: 1px solid var(--color-border);
-  color: var(--color-text-primary);
-  cursor: pointer;
-  font-size: 1rem;
-  height: 40px;
-  padding: 0 1rem;
-}
-button:disabled {
-  opacity: 0.4;
-  cursor: not-allowed;
-}
-article > p {
-  white-space: pre-wrap;
-}
-article > header{
-  display: flex;
-  align-items: center;
-  gap: 0.5rem;
-  margin-bottom: 1rem;
-}
-article img{
-  border-radius: 50%;
-}
-</style>
---
+++ src/components/PostCard.vue
@@ -0,0 +1,44 @@
+<script setup>
+import {TrashIcon, HeartIcon} from "@heroicons/vue/24/outline";
+import {HeartIcon as filledHeartIcon} from "@heroicons/vue/24/solid";
+
+const props = defineProps({
+    post: {
+        type: Object,
+        required: true,
+        
+    },
+});
+
+const emit = defineEmits(["delete", "like"]);
+
+function deletePost(){
+    emit("delete", props.post.id);
+}
+function like (){
+  emit ("like", props.post.id);
+}
+</script>
+
+<template>
+  <article class="card">
+    <header>
+      <img :src="post.author.avatarUrl" alt="avatar" width="36" height="36" class="avatar"/>
+      <a>{{ post.author.username }}</a>
+    </header>
+    <p>{{ post.content }}</p>
+
+    <footer>
+        <button @click="deletePost" class="btn-icon">
+            <TrashIcon/>
+        </button>
+        <button @click="like" class="btn-icon">
+          
+          <HeartIcon v-if="!post.liked"/>
+          <filledHeartIcon v-if="post.liked"/>
+        </button>
+    </footer>
+
+  </article>
+</template>
+
